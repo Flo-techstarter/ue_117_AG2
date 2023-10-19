9b35f5525300fa83c90ec43ca924c99ffa6f1184
Flo <florian.britz@docc.techstarter.de>
2023-10-19 10:13:35 +0200

    Aufgabe 2 (ohne freiwillig) Push2

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,3 @@
 output "instance_id" {
-  value = aws_instance.test2.id
+  value = aws_instance.test.id
 }
-
-output "instance_id_2_2" {
-  value = aws_instance.test2_2.id
-}
-
-output "instance_id_2_3" {
-  value = aws_instance.test2_3.id
-}
-
