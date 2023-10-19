95426b1540f7bf3ae7122a9ce0019956abdc6867
Flo <florian.britz@docc.techstarter.de>
2023-10-19 09:38:04 +0200

    vollständig aufgabe 2

---
+++ outputs.tf
@@ -0,0 +1,12 @@
+output "instance_id" {
+  value = aws_instance.test2.id
+}
+
+output "instance_id_2_2" {
+  value = aws_instance.test2_2.id
+}
+
+output "instance_id_2_3" {
+  value = aws_instance.test2_3.id
+}
+
